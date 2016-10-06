fe1805524efccbf8535c955ef1c02691badb830a
ace <azatibrakov@gmail.com>
2016-10-06 12:40:34 +0700

    Added 'Travis CI' file for tests

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 
 import PIL.Image
 import click
@@ -9,14 +10,15 @@ from services.data_access import DataAccessObject
 from settings import DATABASE_URL
 from settings import (DST_GROUP_ID, SRC_GROUP_ID, APP_ID, USER_LOGIN, USER_PASSWORD, SCOPE, FORBIDDEN_ALBUMS,
                       DST_ABSPATH, WATERMARK_PATH)
+from tests.test_data_access import UnitTestDataAccess, UnitTestDataAccessExceptions
 
 
-@click.group(invoke_without_command=False)
-def main():
+@click.group(name='run', invoke_without_command=False)
+def run():
     pass
 
 
-@main.command(name='sync')
+@run.command(name='run_sync')
 def sync():
     community_app = CommunityApp(APP_ID, DST_GROUP_ID, USER_LOGIN, USER_PASSWORD, SCOPE)
     images_path = os.path.join(DST_ABSPATH, community_app.community_info['screen_name'])
@@ -24,7 +26,7 @@ def sync():
     community_app.synchronize_and_mark(images_path, watermark)
 
 
-@main.command(name='bot')
+@run.command(name='run_bot')
 def post_bot():
     community_app = CommunityApp(APP_ID, DST_GROUP_ID, USER_LOGIN, USER_PASSWORD, SCOPE)
     images_path = os.path.join(DST_ABSPATH, community_app.community_info['screen_name'])
@@ -36,11 +38,26 @@ def post_bot():
     community_app.post_random_photos_on_community_wall(images_path, **filters)
 
 
-@main.command(name='db')
+@run.command(name='run_db')
 def init_db():
     dao = DataAccessObject(DATABASE_URL)
     Base.metadata.create_all(dao.engine)
 
 
+@click.group(name='test', invoke_without_command=False)
+def test():
+    pass
+
+
+@test.command(name='test_dao')
+def test_data_access():
+    """Tests implemented data access"""
+    suite = unittest.TestLoader().loadTestsFromTestCase(UnitTestDataAccess)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    exc_suite = unittest.TestLoader().loadTestsFromTestCase(UnitTestDataAccessExceptions)
+    unittest.TextTestRunner(verbosity=2).run(exc_suite)
+
+manage = click.CommandCollection(sources=[run, test])
+
 if __name__ == '__main__':
-    main()
+    manage()
